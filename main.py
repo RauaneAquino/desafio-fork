b43e8fa3cfa89c6eff44bf035ca4895d33e820f5
Rauane Aquino <rauaneaquino3@gmail.com>
2025-11-03 19:31:52 -0400

    subindo

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def calcular_media(lista):
 # Função para calcular a mediana
 def calcular_mediana(lista):
     # TODO: ordenar a lista e encontrar o elemento do meio
-    # 💡 Dica: se o tamanho for par, tire a média dos dois elementos centrais
+    # 💡 Dica: se o tamanho for par, tire a média dos dois elementos centraisjghjggj
     pass
 
 
